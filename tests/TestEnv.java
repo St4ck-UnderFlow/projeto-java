4cd0172ded381f08ca0663ad0b8673a0cd0daf1a
LuisHenriqueCO <luishenriqueoliveira254@gmail.com>
2023-06-01 14:42:03 -0300

    Testing Classes Maxwell and Menu

--- tests/TestEnv.java
+++ tests/TestEnv.java
@@ -1,11 +1,19 @@
 package tests;
 
-// import classes.Maxwell;
+import classes.Maxwell;
+import classes.Menu;
 
 public class TestEnv {
     public static void main(String[] args) {
         // Create the instances for tests here
-        // Maxwell newMaxwell = new Maxwell();
-        // System.out.println(newMaxwell.getGoal());
+        Maxwell newMaxwell = new Maxwell();
+        System.out.println(newMaxwell.getGoal());
+        newMaxwell.travel();
+        newMaxwell.die();
+
+        Menu newMenu = new Menu();
+        newMenu.startMenu();
+        newMenu.gameOverScreen();
+        newMenu.gameOverConfirm();
     }
 }
