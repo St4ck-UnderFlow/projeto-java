b12ce030f4a34157c525e93dece827f9d3cf598c
Arthur Mousinho <arthursilvamo296@gmail.com>
2023-06-12 13:50:45 -0300

    creating main file to show menu game

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.text.BreakIterator;
+import java.util.Scanner;
+import classes.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Menu menu = new Menu();
+        menu.startMenu();
+
+        int option = input.nextInt();
+        switch(option) {
+            case 1:
+                System.out.println("game started");
+                break;
+            case 2:
+                System.out.println("goodbye");
+                break; 
+            default:
+                input.close();
+        }
+    }
+}
