4cea42dd7926dc20611af0e940e76f404548aa2d
Arthur Mousinho <arthursilvamo296@gmail.com>
2023-06-13 12:17:13 -0300

    updating game atributes

--- classes/Game.java
+++ classes/Game.java
@@ -1,64 +1,41 @@
 package classes;
 
 public class Game {
-    private City ubub;
-    private City nekika;
-    private City legmod;
-    private City gritestar;
-    private City drogove;
-    private City oldcalia;
-    private City laswatch;
-    private City smalia;
-    private City defalsia;
-    private City aymarLeague;
-    private City kalb;
-    private City vuneseEmpire;
-    private City karhora;
-    private City nargumun;
-    private City bun;
-    private City chandirSultanate;
-    private City kasya;
+    private City ubub = new City("Ubub",false, false, null, null);
+
+    private City nekika = new City("Nekika", false, false, null, null);
+
+    private City legmod = new City("Legmod", false, false, null, null);
+
+    private City gritestar = new City("Gritestar", false, false, null, null);
+
+    private City drogove = new City("Dogrove", false, false, null, null);
+
+    private City oldcalia = new City("Oldcalia", false, false, null, null);
+
+    private City laswatch = new City("Lastwatch", false, false, null, null);
+
+    private City smalia = new City("Smalia", false, false, null, null);
+
+    private City defalsia = new City("Defalsia", true, false, null, null);
+
+    private City aymarLeague = new City("Aymar League", true, false, null, null);
+
+    private City kalb = new City("Kalb", false, false, null, null);
+
+    private City vuneseEmpire = new City("Vunese Empire", true, false, null, null);
+
+    private City karhora = new City("Karhora", false, false, null, null);
+
+    private City nargumun = new City("Nargumun", true, false, null, null);
+
+    private City bun = new City("Bun", true, false, null, null);
+
+    private City chandirSultanate = new City("Chandir Sultanate", false, false, null, null)
+
+    private City kasya = new City("Kasya", false, false, null, null);
         
     public Game() {
 
     }
-
-    public void instanceCities() {
- 
-        this.ubub = new City("Ubub",false, false, null, null);
-
-        this.nekika = new City("Nekika", false, false, null, null);
-
-        this.legmod = new City("Legmod", false, false, null, null);
-
-        this.gritestar = new City("Gritestar", false, false, null, null);
-
-        this.drogove = new City("Dogrove", false, false, null, null);
-
-        this.oldcalia = new City("Oldcalia", false, false, null, null);
-
-        this.laswatch = new City("Lastwatch", false, false, null, null);
-
-        this.smalia = new City("Smalia", false, false, null, null);
-
-        this.defalsia = new City("Defalsia", true, false, null, null);
-
-        this.aymarLeague = new City("Aymar League", true, false, null, null);
-
-        this.kalb = new City("Kalb", false, false, null, null);
-
-        this.vuneseEmpire = new City("Vunese Empire", true, false, null, null);
-
-        this.karhora = new City("Karhora", false, false, null, null);
-
-        this.nargumun = new City("Nargumun", true, false, null, null);
-
-        this.bun = new City("Bun", true, false, null, null);
-
-        this.chandirSultanate = new City("Chandir Sultanate", false, false, null, null);
-
-        this.kasya = new City("Kasya", false, false, null, null);
-
-    }
-
 }
