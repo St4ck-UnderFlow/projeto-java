85a967f7bb2af0f8b51fea053ed4275087ab3ec9
Arthur Mousinho <arthursilvamo296@gmail.com>
2023-06-17 10:00:20 -0300

    fixing bug when input a string on start game menu

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import classes.Game;
@@ -6,31 +7,37 @@ import classes.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         Menu menu = new Menu();
         Game game = new Game();
-
         Maxwell maxwell = new Maxwell(game.ubud);
 
         boolean validInput = true;
         
         while(validInput){
-            menu.startMenu();
-            int option = input.nextInt();
-            switch(option) {
-                case 1:
-                    game.startGame();
-                    menu.optionsMenu(maxwell);
-                    validInput = false;
-                    break;
-                case 2:
-                    System.out.println("Leaving So Soon ???");
-                    validInput = false;
-                    break; 
-                default:
-                    menu.clearTerminal();
-                    System.out.println("This option does not exist, please type the right ones");
-                    break;
+            menu.clearTerminal();
+            try {
+                menu.startMenu();
+                int option = input.nextInt();
+                switch(option) {
+                    case 1:
+                        game.startGame();
+                        menu.optionsMenu(maxwell);
+                        validInput = false;
+                        break;
+                    case 2:
+                        System.out.println("Leaving So Soon ???");
+                        validInput = false;
+                        break; 
+                    default:
+                        menu.clearTerminal();
+                        System.out.println("This option does not exist, please type the right ones");
+                        break;
+                }
+            } catch(InputMismatchException error) {
+                input.next(); // Discard invalid input
+                continue;
             }
         }
     }
